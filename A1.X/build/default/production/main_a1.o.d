5d58286e99dec2a2d666466e6c9d1fede5beac49
IloPinci <joel.klajdi.topulli@gmail.com>
2026-05-04 21:51:35 +0200

    Phase2: Complete Architecture

---
+++ A1.X/build/default/production/main_a1.o.d
@@ -0,0 +1,4 @@
+build/default/production/main_a1.o: main_a1.c \
+ ../../EX/my-libraries/timer_config.h
+
+../../EX/my-libraries/timer_config.h:
--- A1.X/main_a1.c
+++ A1.X/main_a1.c
@@ -11,22 +11,40 @@
 #include "timer_config.h"
 #include <math.h>
 
-/*
- * 
- */
 
-#define BUF_SIZE 16  
+#define BUF_SIZE 64 
 #define PI 3.14159265358979323846
-volatile char buffer[BUF_SIZE];
-volatile int head = 0;
-volatile int tail = 0;
+
+volatile char receive_buffer[BUF_SIZE];
+volatile int receive_head = 0, receive_tail = 0;
+
+volatile char transmit_buffer[BUF_SIZE];
+volatile int transmit_head = 0, transmit_tail = 0;
+
 volatile int accel_read_flag = 0;
+volatile int accel_count = 0;
+volatile int char_index  = 0;
 
+volatile int timer_count = 0;
+volatile int ang_count = 0;
 
+volatile int hz_period = 10;   // default 10 Hz so every 10 loops
+volatile int hz_count  = 0;
+
+volatile int rx_debug = 0;
+
+typedef struct{
+    int16_t axis_x;
+    int16_t axis_y;
+    int16_t axis_z;
+
+    float roll;
+    float pitch;
+} EulerAngles;
 
 // we change the read flag allowing for the reading of the accel values
 void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
-    accel_read_flag = 0;
+    accel_read_flag = 1;
     IFS0bits.T3IF = 0; 
     IEC0bits.T3IE = 0;          // also we don't allow interrupts unless the accel_bw is called again
 }
@@ -34,20 +52,30 @@ void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
 // This is for reading from the uart
 void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void) {
     IFS0bits.U1RXIF = 0;
+    rx_debug++;
     
     while (U1STAbits.URXDA == 1) {
         char value = U1RXREG;
         
-        int next = (head + 1) % BUF_SIZE;  // '%' is used to wrap around
-        if (next != tail) {
-            buffer[head] = value;
-            head = next;
-        }
-        
+        int next = (receive_head + 1) % BUF_SIZE;  // '%' is used to wrap around
+        if (next != receive_tail) {
+            receive_buffer[receive_head] = value;
+            receive_head = next;
+        }   
     }
-    
 }
 
+// to transmit 
+void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void) {
+    IFS0bits.U1TXIF = 0;
+
+    if (transmit_tail != transmit_head) {
+        U1TXREG = transmit_buffer[transmit_tail];
+        transmit_tail = (transmit_tail + 1) % BUF_SIZE;
+    } else {
+        IEC0bits.U1TXIE = 0;       // nothing left so we disable interrupt
+    }
+}
 
 
 void initial_setup(){
@@ -85,8 +113,8 @@ void uart_setup(){
     U1STAbits.UTXEN = 1;
     
     // Enable transmitter
-    IFS0bits.U1RXIF = 0;   
-    IEC0bits.U1RXIE = 1;
+    IFS0bits.U1RXIF = 0;    // interrupt flag
+    IEC0bits.U1RXIE = 1;    // interrupt enable
 }
 
 void spi_setup(){
@@ -121,10 +149,10 @@ void spi_setup(){
     TRISDbits.TRISD6 = 0;       // for magnetometer     CS3
     TRISBbits.TRISB4 = 0;       // for gyroscope        CS2
     TRISBbits.TRISB3 = 0;       // for accelerometer    CS1
-    // now we deactivate them 
-    LATDbits.LATD6 = 1;         // bc pulling them up deactivates them
-    LATBbits.LATB4 = 1;
-    LATBbits.LATB3 = 1;
+    // now we deactivate them bc pulling them up deactivates them
+    LATDbits.LATD6 = 1;         // mag
+    LATBbits.LATB4 = 1;         // gyro
+    LATBbits.LATB3 = 1;         // accel
     
 }
 
@@ -140,8 +168,20 @@ unsigned int spi_write(unsigned int data){
     return SPI1BUF;     // return the data we got from the slave
 }
 
-void uart_transmit(char message){
-    int x;
+void uart_transmit(const char* message){
+    while (*message) {
+        int next = (transmit_head + 1) % BUF_SIZE;
+        while (next == transmit_tail);  // wait if buffer full
+        transmit_buffer[transmit_head] = *message++;
+        transmit_head = next;
+    }
+    // Manually send first byte to kick off the interrupt chain
+    if (transmit_tail != transmit_head) {
+        IEC0bits.U1TXIE = 0;                // disable interrupt briefly
+        U1TXREG = transmit_buffer[transmit_tail];
+        transmit_tail = (transmit_tail + 1) % BUF_SIZE;
+        IEC0bits.U1TXIE = 1;                // now enable ? rest sent by interrupt
+    }
 }
 
 // for the badwidth of the filter
@@ -156,14 +196,14 @@ void accel_bw(int bw){
     
     switch(bw) {
     // each filtering frequency requires a different time to update before new data can be considered
-        case 8:  spi_write(0x08); delay_ms = 1; break;
-        case 9:  spi_write(0x09); delay_ms = 1; break;
-        case 10: spi_write(0x0A); delay_ms = 2; break;
-        case 11: spi_write(0x0B); delay_ms = 4; break;
-        case 12: spi_write(0x0C); delay_ms = 8; break;
-        case 13: spi_write(0x0D); delay_ms = 16; break;
-        case 14: spi_write(0x0E); delay_ms = 32; break;
-        case 15: spi_write(0x0F); delay_ms = 64; break;
+        case 8:  spi_write(0x08); delay_ms = 64; break;
+        case 9:  spi_write(0x09); delay_ms = 32; break;
+        case 10: spi_write(0x0A); delay_ms = 16; break;
+        case 11: spi_write(0x0B); delay_ms = 8; break;
+        case 12: spi_write(0x0C); delay_ms = 4; break;
+        case 13: spi_write(0x0D); delay_ms = 2; break;
+        case 14: spi_write(0x0E); delay_ms = 1; break;
+        case 15: spi_write(0x0F); delay_ms = 1; break;
         default: 
             // if a wrong value is sent we send to uart and return 
             LATBbits.LATB3 = 1;
@@ -177,57 +217,145 @@ void accel_bw(int bw){
     IEC0bits.T3IE = 1;          // we enable the interrupt   
 }
 
-char accel_axis(){
-    char euler[3];
+
+// for the frequency of the uart
+void uart_frequency_change(int value){
+
+    switch(value) {
+        case 0:  hz_period = 0;   break;
+        case 1:  hz_period = 100; break;
+        case 2:  hz_period = 50;  break;
+        case 5:  hz_period = 20;  break;
+        case 10: hz_period = 10;  break;
+        default:
+            uart_transmit("$ERR,2*");
+            break;
+    }    
+}
+
+EulerAngles accel_axis(){
+    EulerAngles result;
+
     uint16_t LSB_part, MSB_part;
-    double axis_x, axis_y, axis_z ;
     
+
     // x-axis starts at 0x42 ends at 0x43
-    LATDbits.LATD6 = 0;
+    LATBbits.LATB3 = 0;
     spi_write(0x42 | 0x80); 
     LSB_part = spi_write(0x00);
     MSB_part = spi_write(0x00);         // it is auto incremented     
     
-    axis_x = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
-    
+    result.axis_x = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
     
     // y-axis starts at 0x44 ends at 0x45
     LSB_part = spi_write(0x00);
     MSB_part = spi_write(0x00);           
-    axis_y = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
+    result.axis_y = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
     
     // z-axis starts at 0x46 ends at 0x47
     LSB_part = spi_write(0x00);
     MSB_part = spi_write(0x00);         
-    axis_z = ((int16_t)((MSB_part << 8) | LSB_part)) >> 1;   // z-axis has 15 bits
+    result.axis_z = ((int16_t)((MSB_part << 8) | LSB_part)) >> 1;   // z-axis has 15 bits
+    LATBbits.LATB3 = 1;
     
-    LATDbits.LATD6 = 1;
+    // angle calculation
+    result.roll = atan2(result.axis_y, result.axis_z) * (180.0 / PI);  // roll in degrees
+    result.pitch = atan2(-result.axis_x, sqrt((result.axis_y * result.axis_y) + (result.axis_z * result.axis_z))) * (180.0 / PI); //pitch
     
-    
-    euler[1] = atan2(axis_y, axis_z) / (180 / PI);  // roll in degrees
-    euler[2] = atan2(-axis_x, sqrt((axis_y * axis_y) + (axis_z * axis_z))) / (180 / PI); //pitch
-    euler[3] = axis_z;      // the z axis just for completness
-    
-    return euler;
+    return result;
 }
-
 
 
 int main() {
     int period_misses = 0;
-    int timer_count = 0;
+    char reciv_char[7] = {0, 0, 0, 0, 0, 0, 0};;
+    int value = 0;
+    EulerAngles euler_angles;
+    char buffer_euler[48];
     
     initial_setup();
     uart_setup();
     spi_setup();
+    accel_read_flag = 1;
     accel_bw(15);
     
     
     while(1){
         algorithm();
+
+        // we have to get the accelerometer data every 20 ms so every two iterations
+        if (++accel_count >= 2 && accel_read_flag == 1){    // we make sure that the data is filtered correctly
+            accel_count = 0;
+            euler_angles =  accel_axis();
+
+            // send the angle axes
+            if (hz_period > 0 && ++hz_count >= hz_period) {
+                hz_count = 0;
+                sprintf(buffer_euler, "$ACC,%d,%d,%d*",
+                (int)euler_angles.axis_x,
+                (int)euler_angles.axis_y,
+                (int)euler_angles.axis_z);
+
+                uart_transmit(buffer_euler);
+            }
+        }
+
+        // for the angles 
+        if (++ang_count >= 20) {
+            ang_count = 0;
+            sprintf(buffer_euler, "$ANG,%.2f,%.2f,RX:%d**", (double)euler_angles.roll, (double)euler_angles.pitch, rx_debug);
+            uart_transmit(buffer_euler);
+        }
+        
+        while (receive_tail != receive_head){
+           char tempChar = receive_buffer[receive_tail];
+           receive_tail = (receive_tail+1) % BUF_SIZE;
+           
+           reciv_char[0] = reciv_char[1];
+           reciv_char[1] = reciv_char[2];
+           reciv_char[2] = reciv_char[3];
+           reciv_char[3] = reciv_char[4];
+           reciv_char[4] = reciv_char[5];
+           reciv_char[5] = reciv_char[6];
+           reciv_char[6] = tempChar;
+           
+            if (char_index  < 7){
+                char_index ++;
+            }
+           
+           if (char_index  == 7){          
+            
+//            sprintf(buffer_euler, "$DBG,%d,%d,%d,%d,%d,%d,%d*",
+//            (int)reciv_char[0], (int)reciv_char[1], (int)reciv_char[2],
+//            (int)reciv_char[3], (int)reciv_char[4], (int)reciv_char[5],
+//            (int)reciv_char[6]);
+//            uart_transmit(buffer_euler);
+            
+
+                if (reciv_char[0] == '$' && reciv_char[3] == ',' && reciv_char[6] == '*' &&
+                   ((reciv_char[1] == 'H' &&  reciv_char[2] == 'Z') ||
+                   (reciv_char[1] == 'B' && reciv_char[2] == 'W')) &&
+                   (reciv_char[4] >= '0' && reciv_char[4] <= '9') &&
+                   (reciv_char[5] >= '0' && reciv_char[5] <= '9')){ 
+                    
+                    value = (reciv_char[4] - '0') * 10 + (reciv_char[5] - '0'); // we turn chars into numbers
+                    // FOR UART DATA FREQUENCY
+                    if (reciv_char[1] == 'H' &&  reciv_char[2] == 'Z'){
+                        uart_frequency_change(value);
+                    }
+                    // FOR BANDWIDTH FILTER CONTROL
+                    else{
+                       accel_bw(value);
+                    }
+                    reciv_char[0] = reciv_char[1] = reciv_char[2] = reciv_char[3] = 0;
+                    reciv_char[4] = reciv_char[5] = reciv_char[6] = 0;
+                    char_index = 0;
+                }
+            } 
+       }               
         
         
-        
+            
         
         if(!tmr_wait_period(TIMER1)){
             period_misses++;
