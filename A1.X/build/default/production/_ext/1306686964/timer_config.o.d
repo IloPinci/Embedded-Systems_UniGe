c72994731dfbd8cdf335fe3d3182ac174d133f50
IloPinci <joel.klajdi.topulli@gmail.com>
2026-05-04 14:23:42 +0200

    Phase 1: LED/BandWidth

---
+++ A1.X/build/default/production/_ext/1306686964/timer_config.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1306686964/timer_config.o: \
+ ../../EX/my-libraries/timer_config.c \
+ ../../EX/my-libraries/timer_config.h
+
+../../EX/my-libraries/timer_config.h:
---
+++ A1.X/main_a1.c
@@ -0,0 +1,243 @@
+/* 
+ * File:   main_a1.c
+ * Author: JT
+ *
+ * Created on April 26, 2026, 7:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "xc.h"
+#include "timer_config.h"
+#include <math.h>
+
+/*
+ * 
+ */
+
+#define BUF_SIZE 16  
+#define PI 3.14159265358979323846
+volatile char buffer[BUF_SIZE];
+volatile int head = 0;
+volatile int tail = 0;
+volatile int accel_read_flag = 0;
+
+
+
+// we change the read flag allowing for the reading of the accel values
+void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
+    accel_read_flag = 0;
+    IFS0bits.T3IF = 0; 
+    IEC0bits.T3IE = 0;          // also we don't allow interrupts unless the accel_bw is called again
+}
+
+// This is for reading from the uart
+void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void) {
+    IFS0bits.U1RXIF = 0;
+    
+    while (U1STAbits.URXDA == 1) {
+        char value = U1RXREG;
+        
+        int next = (head + 1) % BUF_SIZE;  // '%' is used to wrap around
+        if (next != tail) {
+            buffer[head] = value;
+            head = next;
+        }
+        
+    }
+    
+}
+
+
+
+void initial_setup(){
+    // disable the analog modality
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+    
+    // timers
+    tmr_setup_period(TIMER1, 10);
+    tmr_setup_period(TIMER2, 10);
+    
+    // LED2
+    TRISGbits.TRISG9 = 0;       // output
+    LATGbits.LATG9 = 1;         // initial value
+}
+
+void algorithm(){
+    tmr_wait_ms(TIMER2, 7);
+}
+
+void uart_setup(){
+    // I/O for Uart
+    TRISDbits.TRISD11 = 1;      // in
+    TRISDbits.TRISD0 = 0;       // out   
+    
+    // Pin remapping
+    RPINR18bits.U1RXR = 75;         // for input which is supposed to be the RD11 pin        
+    RPOR0bits.RP64R = 0x01;         //  for output which is supposed to be the RD0        
+    
+    // Baud Rate Setup
+    U1MODEbits.BRGH = 0;    // to select the 16 divisor
+    U1BRG = 468;            // we load it so we can get 9600
+    
+    // Power on the module
+    U1MODEbits.UARTEN = 1;
+    U1STAbits.UTXEN = 1;
+    
+    // Enable transmitter
+    IFS0bits.U1RXIF = 0;   
+    IEC0bits.U1RXIE = 1;
+}
+
+void spi_setup(){
+    SPI1STATbits.SPIEN = 0;             // we disable before we do changes (just to be sure)
+    
+    // I/O setup
+    TRISAbits.TRISA1 = 1;               // RA1 corresponds to RPI17 and we set it as input for master-in
+    TRISFbits.TRISF12 = 0;              // RF12 corresponds to RP108 SCK and we set it as output for the clock
+    TRISFbits.TRISF13 = 0;              // RF13 corresponds to RP109 and we set it as output for master-out
+    
+    // Pin remapping
+    RPINR20bits.SDI1R = 0b0010001;      // MISO (SDI1) - RPI17 [RA1]
+    RPOR12bits.RP109R = 0b000101;       // MOSI (SDO1) - RP109 [RF13]  
+    RPOR11bits.RP108R = 0b000110;       // SCK1 [RF12] - RP108 [RF12]
+
+    // Modality setup
+    SPI1CON1bits.MSTEN = 1;     // enable master
+    SPI1CON1bits.MODE16 = 0;    // 8bit modality
+    SPI1CON1bits.CKP = 1;       // set the clock idle to 1
+    
+    // to match the operating frequency of the slave
+    SPI1CON1bits.PPRE = 3;       // primary presacler 1:1
+    SPI1CON1bits.SPRE = 3;       // secondary prescaler 3:1
+    
+    SPI1STATbits.SPIROV = 0;    // clear overflow flag 
+    SPI1STATbits.SPIEN = 1;     // enable spi  
+    
+    
+    // Chip Select Setup
+    
+    // define them as output
+    TRISDbits.TRISD6 = 0;       // for magnetometer     CS3
+    TRISBbits.TRISB4 = 0;       // for gyroscope        CS2
+    TRISBbits.TRISB3 = 0;       // for accelerometer    CS1
+    // now we deactivate them 
+    LATDbits.LATD6 = 1;         // bc pulling them up deactivates them
+    LATBbits.LATB4 = 1;
+    LATBbits.LATB3 = 1;
+    
+}
+
+unsigned int spi_write(unsigned int data){
+    while (SPI1STATbits.SPITBF == 1);    // wait until the t-buffer is emptied 
+    // ^ can be occupied by previous iterations so we wait until it is cleared
+    
+    SPI1BUF = data;                      // write data (also triggers the clock)
+    
+    while (SPI1STATbits.SPIRBF == 0);    // wait until the whole transmission is complete
+    // ^ ensures that the whole word is sent
+    
+    return SPI1BUF;     // return the data we got from the slave
+}
+
+void uart_transmit(char message){
+    int x;
+}
+
+// for the badwidth of the filter
+void accel_bw(int bw){
+    
+    int delay_ms = 0;
+    accel_read_flag = 0;
+    
+    // it boots in normal mode the moment it is selected
+    LATBbits.LATB3 = 0;         // we activate the chip
+    spi_write(0x10 & 0x7F);     // the register to configure the bandwidth
+    
+    switch(bw) {
+    // each filtering frequency requires a different time to update before new data can be considered
+        case 8:  spi_write(0x08); delay_ms = 1; break;
+        case 9:  spi_write(0x09); delay_ms = 1; break;
+        case 10: spi_write(0x0A); delay_ms = 2; break;
+        case 11: spi_write(0x0B); delay_ms = 4; break;
+        case 12: spi_write(0x0C); delay_ms = 8; break;
+        case 13: spi_write(0x0D); delay_ms = 16; break;
+        case 14: spi_write(0x0E); delay_ms = 32; break;
+        case 15: spi_write(0x0F); delay_ms = 64; break;
+        default: 
+            // if a wrong value is sent we send to uart and return 
+            LATBbits.LATB3 = 1;
+            uart_transmit("$ERR,1*");
+            return;
+    }
+    LATBbits.LATB3 = 1;  
+    
+    tmr_setup_period(TIMER3, delay_ms);
+    IFS0bits.T3IF = 0;          // we ensure that the flag is clean before we enable the interrupt
+    IEC0bits.T3IE = 1;          // we enable the interrupt   
+}
+
+char accel_axis(){
+    char euler[3];
+    uint16_t LSB_part, MSB_part;
+    double axis_x, axis_y, axis_z ;
+    
+    // x-axis starts at 0x42 ends at 0x43
+    LATDbits.LATD6 = 0;
+    spi_write(0x42 | 0x80); 
+    LSB_part = spi_write(0x00);
+    MSB_part = spi_write(0x00);         // it is auto incremented     
+    
+    axis_x = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
+    
+    
+    // y-axis starts at 0x44 ends at 0x45
+    LSB_part = spi_write(0x00);
+    MSB_part = spi_write(0x00);           
+    axis_y = ((int16_t)((MSB_part << 8) | LSB_part)) >> 3;
+    
+    // z-axis starts at 0x46 ends at 0x47
+    LSB_part = spi_write(0x00);
+    MSB_part = spi_write(0x00);         
+    axis_z = ((int16_t)((MSB_part << 8) | LSB_part)) >> 1;   // z-axis has 15 bits
+    
+    LATDbits.LATD6 = 1;
+    
+    
+    euler[1] = atan2(axis_y, axis_z) / (180 / PI);  // roll in degrees
+    euler[2] = atan2(-axis_x, sqrt((axis_y * axis_y) + (axis_z * axis_z))) / (180 / PI); //pitch
+    euler[3] = axis_z;      // the z axis just for completness
+    
+    return euler;
+}
+
+
+
+int main() {
+    int period_misses = 0;
+    int timer_count = 0;
+    
+    initial_setup();
+    uart_setup();
+    spi_setup();
+    accel_bw(15);
+    
+    
+    while(1){
+        algorithm();
+        
+        
+        
+        
+        if(!tmr_wait_period(TIMER1)){
+            period_misses++;
+        }
+        if (++timer_count == 50){
+            LATGbits.LATG9 = !LATGbits.LATG9;
+            timer_count = 0;
+        }
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
