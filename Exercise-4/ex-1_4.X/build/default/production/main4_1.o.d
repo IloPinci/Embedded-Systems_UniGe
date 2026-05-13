2fb63a70bac829c7459354f4d840dbbaa19e0a98
IloPinci <133425186+IloPinci@users.noreply.github.com>
2026-05-13 15:27:04 +0200

    Delete Exercise-4 directory

--- Exercise-4/ex-1_4.X/build/default/production/main4_1.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/main4_1.o: main4_1.c \
- ../../my-libraries/timer_config.h
-
-../../my-libraries/timer_config.h:
--- Exercise-4/ex2_4.X/build/default/production/main4_2.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/main4_2.o: main4_2.c \
- ../../my-libraries/timer_config.h
-
-../../my-libraries/timer_config.h:
